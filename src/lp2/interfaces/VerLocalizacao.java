dc3315ec84bd0c18d099258fd1c3ce8ddaeb7cf0
irvile turin <irvile.turin@gmail.com>
2011-06-05 19:26:10 -0300

    Com novo tipo de vis?o do mapa.E quase com zoom. =] p.s: mais 2 imagens do botao + zoom e - zoom.

--- src/lp2/interfaces/VerLocalizacao.java
+++ src/lp2/interfaces/VerLocalizacao.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import lp2.lerDados.Estabelecimento;
 import lp2.lerDados.ReadData;
@@ -34,7 +35,7 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 	String urlFinal;
 	boolean selecionadaVisaoSatelite = false;
 	boolean selecionadaVisaoHibrida = false;
-	boolean selecionadaVisaoEstrada = false;
+	boolean selecionadaVisaoTerreno = false;
 	boolean selecionadaVisaoNormal = false;
 
 	JFrame frameMostraMapa;
@@ -53,7 +54,7 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 	String link;
 	private JRadioButton tipoVisaoSatelite;
 	private JRadioButton tipoVisaoHibrida;
-	private JRadioButton tipoVisaoEstradas;
+	private JRadioButton tipoVisaoTerreno;
 	private JRadioButton tipoVisaoNormal;
 	private ButtonGroup selecionaVisao;
 
@@ -71,43 +72,25 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 		endereco = new JTextField();
 		tipoVisaoSatelite = new JRadioButton("Visao Satelite");
 		tipoVisaoHibrida = new JRadioButton("Visao Hibrida");
-		tipoVisaoEstradas = new JRadioButton("Visao Estradas");
+		tipoVisaoTerreno = new JRadioButton("Visao Terrena");
 		tipoVisaoNormal = new JRadioButton("Visao Normal");
 		selecionaVisao = new ButtonGroup();
 		selecionaVisao.add(tipoVisaoHibrida);
-		selecionaVisao.add(tipoVisaoEstradas);
+		selecionaVisao.add(tipoVisaoTerreno);
 		selecionaVisao.add(tipoVisaoNormal);
 		selecionaVisao.add(tipoVisaoSatelite);
 
 		visualizaMapa = new JPanel();
 
 		iniciaComboBoxEstabelecimentos();
-
-		//		imagemCarregando = new CarregaImagem();
-		//		carregandoThread = new Thread(imagemCarregando);
-		//		carregandoThread.start();
-		//buffImgMapa = getImage(linkDefault);
-		//imageMapa = new JLabel(new ImageIcon(buffImgMapa));
-		//visualizaMapa.add(imageMapa);
-
-		//		getContentPane().add(insiraLocal, new AbsoluteConstraints(50,50,123,23));
-		//		getContentPane().add(endereco, new AbsoluteConstraints(50,80,200,25));
-		//		getContentPane().add(ondeDesejaIr, new AbsoluteConstraints(50,120,123,23));
-		//		getContentPane().add(listaEstabelecimetosCadastrados, new AbsoluteConstraints(50,150,320,23));
-		//		getContentPane().add(botaoLocalizar, new AbsoluteConstraints(50,220,123,23));
-		//		getContentPane().add(botaoVoltar, new AbsoluteConstraints(600,500,120,23));
-		//		getContentPane().add(tipoVisaoEstradas,new AbsoluteConstraints(400,50,200,25));
-		//		getContentPane().add(tipoVisaoHibrida,new AbsoluteConstraints(400,75,200,25));
-		//		getContentPane().add(tipoVisaoSatelite,new AbsoluteConstraints(400,100,200,25));
-		//		getContentPane().add(tipoVisaoNormal,new AbsoluteConstraints(400,120,200,25));
-		//		
+	
 		add(insiraLocal, new AbsoluteConstraints(50,50,123,23));
 		add(endereco, new AbsoluteConstraints(50,80,200,25));
 		add(ondeDesejaIr, new AbsoluteConstraints(50,120,123,23));
 		add(listaEstabelecimetosCadastrados, new AbsoluteConstraints(50,150,320,23));
 		add(botaoLocalizar, new AbsoluteConstraints(50,220,123,23));
 		add(botaoVoltar, new AbsoluteConstraints(600,500,120,23));
-		add(tipoVisaoEstradas,new AbsoluteConstraints(400,50,200,25));
+		add(tipoVisaoTerreno,new AbsoluteConstraints(400,50,200,25));
 		add(tipoVisaoHibrida,new AbsoluteConstraints(400,75,200,25));
 		add(tipoVisaoSatelite,new AbsoluteConstraints(400,100,200,25));
 		add(tipoVisaoNormal,new AbsoluteConstraints(400,120,200,25));
@@ -115,7 +98,7 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 
 		endereco.addKeyListener(new KeyAdapter() {
 			public void keyReleased(KeyEvent evt) {
-				System.out.println(endereco.getText());
+				//System.out.println(endereco.getText());
 			}
 		});
 
@@ -123,7 +106,7 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 		listaEstabelecimetosCadastrados.addActionListener(this);
 		botaoLocalizar.addActionListener(this);
 		botaoVoltar.addActionListener(this);
-		tipoVisaoEstradas.addActionListener(this);
+		tipoVisaoTerreno.addActionListener(this);
 		tipoVisaoHibrida.addActionListener(this);
 		tipoVisaoNormal.addActionListener(this);
 		tipoVisaoSatelite.addActionListener(this);
@@ -153,16 +136,7 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 		return null;
 
 	}
-	//	
-	//	public static String fazURLImageNormal(String lugar1,String lugar2){
-	//		String fonte = lugar1.replace(" ", "+") + ",Campina+Grande,BR";
-	//		String destino = lugar2.replace(" ", "+") + ",Campina+Grande,BR";
-	//		String completa = fonte + "|";
-	//		completa += destino;
-	//		String link = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + completa + "&mobile=true&sensor=false";
-	//		return link;
-	//	}
-	//	
+	
 	public static String concatenaDestinos(String lugar1,String lugar2){
 		String fonte = lugar1.replace(" ", "+") + ",Campina+Grande,BR";
 		String destino = lugar2.replace(" ", "+") + ",Campina+Grande,BR";
@@ -176,13 +150,22 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&maptype=satellite&sensor=false";
 		}if(tipo.equals("Hibrida")){
 			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&maptype=hybrid&sensor=false";
-		}if(tipo.equals("Estrada")){
-			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&maptype=roadmap&sensor=false";
 		}if(tipo.equals("Normal")){
-			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&sensor=false";
+			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&maptype=roadmap&sensor=false";
+		}if(tipo.equals("Terreno")){
+			return setURL = "http://maps.google.com/maps/api/staticmap?&zoom=14&size=600x512&markers=size:m|color:red|" + fonteDestino + "&mobile=true&maptype=terrain&sensor=false";
 		}
 		return setURL;
 	}
+	
+	
+	public static String zoom(String url, int zoom){
+		String setURL = "";
+		setURL = url.replace(url.substring(42, 50),"zoom=" + String.valueOf(zoom));
+		return setURL;
+	}
+	
+	
 
 	private void iniciaComboBoxEstabelecimentos() {
 		//tamanho da lista de estabelecimentos + 1,pq logo abaixo eh adicionado um espaco vazio.
@@ -198,11 +181,6 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 
 	}
 
-	//	public static void main(String args[]){
-	//		VerLocalizacao ver = new VerLocalizacao();
-	//		ver.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//	}
-
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		if(event.getSource() == botaoLocalizar){
@@ -210,45 +188,44 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 				String fonteDestino = concatenaDestinos(endereco.getText(), enderecoDestino);
 				if(selecionadaVisaoNormal){
 					urlFinal = setTipoImagem("Normal", fonteDestino);
-					System.out.println(urlFinal);
+					//System.out.println(urlFinal);
 					criaFrame(urlFinal);
-				}else if(selecionadaVisaoEstrada){
-					urlFinal = setTipoImagem("Estrada", fonteDestino);
-					System.out.println(urlFinal);
+				}else if(selecionadaVisaoTerreno){
+					urlFinal = setTipoImagem("Terreno", fonteDestino);
+					//System.out.println(urlFinal);
 					criaFrame(urlFinal);
 				}else if(selecionadaVisaoHibrida){
 					urlFinal = setTipoImagem("Hibrida", fonteDestino);
-					System.out.println(urlFinal);
+					//System.out.println(urlFinal);
 					criaFrame(urlFinal);
 				}else if(selecionadaVisaoSatelite){
 					urlFinal = setTipoImagem("Satelite", fonteDestino);
-					System.out.println(urlFinal);
+					//System.out.println(urlFinal);
 					criaFrame(urlFinal);
 				}else
 					JOptionPane.showMessageDialog(null, "Escolha enderecos/Tipo de Visao", "Erro", JOptionPane.ERROR_MESSAGE);
 			}else{
-				 JOptionPane.showMessageDialog(null, "Escolha enderecos/Tipo de Visao", "Erro", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Escolha enderecos/Tipo de Visao", "Erro", JOptionPane.ERROR_MESSAGE);
 			}
 
 		}
 		if(event.getSource() == tipoVisaoNormal){
-			System.out.println("tipo normal");
 			selecionadaVisaoNormal = true;
 			selecionadaVisaoSatelite = false;
 			selecionadaVisaoHibrida = false;
-			selecionadaVisaoEstrada = false;
+			selecionadaVisaoTerreno = false;
 		}if(event.getSource() == tipoVisaoHibrida){
 			selecionadaVisaoHibrida = true;
 			selecionadaVisaoSatelite = false;
-			selecionadaVisaoEstrada = false;
+			selecionadaVisaoTerreno = false;
 			selecionadaVisaoNormal = false;
 		}if(event.getSource() == tipoVisaoSatelite){
 			selecionadaVisaoSatelite = true;
 			selecionadaVisaoHibrida = false;
-			selecionadaVisaoEstrada = false;
+			selecionadaVisaoTerreno = false;
 			selecionadaVisaoNormal = false;
-		}if(event.getSource() == tipoVisaoEstradas){
-			selecionadaVisaoEstrada = true;
+		}if(event.getSource() == tipoVisaoTerreno){
+			selecionadaVisaoTerreno = true;
 			selecionadaVisaoSatelite = false;
 			selecionadaVisaoHibrida = false;
 			selecionadaVisaoNormal = false;
@@ -271,21 +248,52 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 			MenuInicial.panelCorpo.updateUI();
 		}
 	}
-	private void criaFrame(String URL){
-		BufferedImage imgCarregada;
+	private void criaFrame(final String URL){
+		
+		BufferedImage imgCarregada = null;
 		final JFrame frame = new JFrame("Bom Conselho no Google Mapa");
+		JLabel iconMaisZoom = new JLabel(new ImageIcon("./src/lp2/imagens/maisZoom.png"));
+		JLabel iconMenosZoom = new JLabel(new ImageIcon("./src/lp2/imagens/menosZoom.png"));
+		JLabel imgFundo = null;
+		final JSlider controlaZoom = new JSlider(SwingConstants.VERTICAL,0,21,14);
+		controlaZoom.setMajorTickSpacing(1);
 		frame.setSize(605,512);
 		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
+		frame.setLayout(new AbsoluteLayout());
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().add(iconMaisZoom, new AbsoluteConstraints(10, 10,-1, 30));
+		frame.getContentPane().add(iconMenosZoom, new AbsoluteConstraints(10, 160, -1, 30));
+		frame.getContentPane().add(controlaZoom, new AbsoluteConstraints(14,34,20,130));
 		try{
-		imgCarregada = getImage(URL);
-		JLabel imgLabel = new JLabel(new ImageIcon(imgCarregada));
-		frame.setContentPane(imgLabel);
-		frame.setVisible(true);
+			imgCarregada = getImage(URL);
 		}catch(NullPointerException ex){
 			ex.getStackTrace();
 		}
+		
+		imgFundo = new JLabel(new ImageIcon(imgCarregada));
+		frame.getContentPane().add(imgFundo, new AbsoluteConstraints(-1, -2, -1, -1));
+		frame.setVisible(true);
+		
+		controlaZoom.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent arg0) {
+				
+			}
+		});
+		
+		
 	}
+//	private BufferedImage atualizaMapa(String URL,int zoom){
+//		BufferedImage imgCarregada;
+//		String novaURL;
+//		try{
+//			novaURL = aumentaZoom(URL, zoom);
+//			imgCarregada = getImage(novaURL);
+//			return imgCarregada;
+//		}catch(NullPointerException ex){
+//			ex.getStackTrace();
+//		}
+//		return null;
+//	}
 
 }
