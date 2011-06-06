09a2d51777c5d2c2413c8917d39b050e6fc61705
irvile turin <irvile.turin@gmail.com>
2011-06-06 12:48:45 -0300

    Uso de Thread pra maximizar o desempenho =] Aew galerinha, com zoom agora.Muito iiiraddooo.

--- src/lp2/interfaces/VerLocalizacao.java
+++ src/lp2/interfaces/VerLocalizacao.java
@@ -21,6 +21,7 @@ import lp2.lerDados.ReadData;
 import org.netbeans.lib.awtextra.AbsoluteConstraints;
 import org.netbeans.lib.awtextra.AbsoluteLayout;
 
+@SuppressWarnings("serial")
 public class VerLocalizacao extends JPanel implements ActionListener{
 
 	//http://maps.google.com/maps/api/staticmap?&zoom=16&size=600x512&markers=size:m|color:red|Rua+Gilo+Guedes,Campina+Grande,BR|Rua+Santa+Cecilia,Campina+Grande+BR&mobile=true&maptype=hybrid&sensor=false
@@ -113,13 +114,45 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 	}
 
 
-
-	//	class CarregaImagem extends Thread{
-	//		
-	//		public void run(){
-	//			buffImgMapa = getImage(linkDefault);
-	//		}
-	//	}
+		class CarregaImagem extends Thread{
+			String url;
+			BufferedImage imgCarregada = null;
+			final JFrame frame = new JFrame("Bom Conselho no Google Mapa");
+			JLabel iconMaisZoom = new JLabel(new ImageIcon("./src/lp2/imagens/maisZoom.png"));
+			JLabel iconMenosZoom = new JLabel(new ImageIcon("./src/lp2/imagens/menosZoom.png"));
+			JLabel imgFundo = null;
+			final JSlider controlaZoom = new JSlider(SwingConstants.VERTICAL,0,21,14);
+			
+			public CarregaImagem(final String url){
+				this.url = url;
+				controlaZoom.addChangeListener(new ChangeListener() {
+				@Override
+					public void stateChanged(ChangeEvent arg0) {
+						imgFundo.setIcon(new ImageIcon(getImage(zoom(url, controlaZoom.getValue()))));
+						frame.validate();
+					}
+				});
+				
+			}
+			public void run(){
+				imgCarregada = getImage(url);
+				frame.setSize(605,512);
+				frame.setResizable(false);
+				frame.setLayout(new AbsoluteLayout());
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				controlaZoom.setMajorTickSpacing(1);
+				frame.getContentPane().add(iconMaisZoom, new AbsoluteConstraints(10, 10,-1, 30));
+				frame.getContentPane().add(iconMenosZoom, new AbsoluteConstraints(10, 160, -1, 30));
+				frame.getContentPane().add(controlaZoom, new AbsoluteConstraints(14,34,20,130));
+				imgFundo = new JLabel(new ImageIcon(imgCarregada));
+				frame.getContentPane().add(imgFundo, new AbsoluteConstraints(-1, -2, -1, -1));
+				frame.setVisible(true);
+			}
+			
+//			public void setImagemLabel(){
+//				imgFundo.setIcon(arg0)
+//			}
+		}
 
 	public static BufferedImage getImage(String caminho) {
 
@@ -189,19 +222,27 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 				if(selecionadaVisaoNormal){
 					urlFinal = setTipoImagem("Normal", fonteDestino);
 					//System.out.println(urlFinal);
-					criaFrame(urlFinal);
+					//criaFrame(urlFinal);
+					Thread th = new Thread(new CarregaImagem(urlFinal));
+					th.start();
 				}else if(selecionadaVisaoTerreno){
 					urlFinal = setTipoImagem("Terreno", fonteDestino);
 					//System.out.println(urlFinal);
-					criaFrame(urlFinal);
+					//criaFrame(urlFinal);
+					Thread th = new Thread(new CarregaImagem(urlFinal));
+					th.start();
 				}else if(selecionadaVisaoHibrida){
 					urlFinal = setTipoImagem("Hibrida", fonteDestino);
 					//System.out.println(urlFinal);
-					criaFrame(urlFinal);
+					//criaFrame(urlFinal);
+					Thread th = new Thread(new CarregaImagem(urlFinal));
+					th.start();
 				}else if(selecionadaVisaoSatelite){
 					urlFinal = setTipoImagem("Satelite", fonteDestino);
 					//System.out.println(urlFinal);
-					criaFrame(urlFinal);
+					//criaFrame(urlFinal);
+					Thread th = new Thread(new CarregaImagem(urlFinal));
+					th.start();
 				}else
 					JOptionPane.showMessageDialog(null, "Escolha enderecos/Tipo de Visao", "Erro", JOptionPane.ERROR_MESSAGE);
 			}else{
@@ -248,41 +289,41 @@ public class VerLocalizacao extends JPanel implements ActionListener{
 			MenuInicial.panelCorpo.updateUI();
 		}
 	}
-	private void criaFrame(final String URL){
-		
-		BufferedImage imgCarregada = null;
-		final JFrame frame = new JFrame("Bom Conselho no Google Mapa");
-		JLabel iconMaisZoom = new JLabel(new ImageIcon("./src/lp2/imagens/maisZoom.png"));
-		JLabel iconMenosZoom = new JLabel(new ImageIcon("./src/lp2/imagens/menosZoom.png"));
-		JLabel imgFundo = null;
-		final JSlider controlaZoom = new JSlider(SwingConstants.VERTICAL,0,21,14);
-		controlaZoom.setMajorTickSpacing(1);
-		frame.setSize(605,512);
-		frame.setResizable(false);
-		frame.setLayout(new AbsoluteLayout());
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().add(iconMaisZoom, new AbsoluteConstraints(10, 10,-1, 30));
-		frame.getContentPane().add(iconMenosZoom, new AbsoluteConstraints(10, 160, -1, 30));
-		frame.getContentPane().add(controlaZoom, new AbsoluteConstraints(14,34,20,130));
-		try{
-			imgCarregada = getImage(URL);
-		}catch(NullPointerException ex){
-			ex.getStackTrace();
-		}
-		
-		imgFundo = new JLabel(new ImageIcon(imgCarregada));
-		frame.getContentPane().add(imgFundo, new AbsoluteConstraints(-1, -2, -1, -1));
-		frame.setVisible(true);
-		
-		controlaZoom.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent arg0) {
-				
-			}
-		});
-		
-		
-	}
+//	private void criaFrame(final String URL){
+//		
+//		BufferedImage imgCarregada = null;
+//		final JFrame frame = new JFrame("Bom Conselho no Google Mapa");
+//		JLabel iconMaisZoom = new JLabel(new ImageIcon("./src/lp2/imagens/maisZoom.png"));
+//		JLabel iconMenosZoom = new JLabel(new ImageIcon("./src/lp2/imagens/menosZoom.png"));
+//		JLabel imgFundo = null;
+//		final JSlider controlaZoom = new JSlider(SwingConstants.VERTICAL,0,21,14);
+//		controlaZoom.setMajorTickSpacing(1);
+//		frame.setSize(605,512);
+//		frame.setResizable(false);
+//		frame.setLayout(new AbsoluteLayout());
+//		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//		frame.getContentPane().add(iconMaisZoom, new AbsoluteConstraints(10, 10,-1, 30));
+//		frame.getContentPane().add(iconMenosZoom, new AbsoluteConstraints(10, 160, -1, 30));
+//		frame.getContentPane().add(controlaZoom, new AbsoluteConstraints(14,34,20,130));
+//		try{
+//			imgCarregada = getImage(URL);
+//		}catch(NullPointerException ex){
+//			ex.getStackTrace();
+//		}
+//		
+//		imgFundo = new JLabel(new ImageIcon(imgCarregada));
+//		frame.getContentPane().add(imgFundo, new AbsoluteConstraints(-1, -2, -1, -1));
+//		frame.setVisible(true);
+//		
+//		controlaZoom.addChangeListener(new ChangeListener() {
+//			@Override
+//			public void stateChanged(ChangeEvent arg0) {
+//				
+//			}
+//		});
+//		
+//		
+//	}
 //	private BufferedImage atualizaMapa(String URL,int zoom){
 //		BufferedImage imgCarregada;
 //		String novaURL;
