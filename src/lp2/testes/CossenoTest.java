9a11ebbf08bed2e7278ba8ee1708db424e75273b
jordaoesa <jordaoesa@localhost>
2011-06-08 00:12:41 -0300

    modificacoes e criacoes de novos testes para os novos algoritmos

--- src/lp2/testes/CossenoTest.java
+++ src/lp2/testes/CossenoTest.java
@@ -3,7 +3,7 @@ package lp2.testes;
 import java.util.ArrayList;
 import java.util.List;
 import junit.framework.Assert;
-import lp2.algoritmos.ProdutoEscalar;
+import lp2.algoritmos.Cosseno;
 import lp2.lerDados.Usuario;
 
 import org.junit.Before;
@@ -17,8 +17,8 @@ import org.junit.Test;
  *         Jordao Ezequiel Serafim de Araujo<br>
  *
  */
-public class ProdutoEscalarTest {
-	ProdutoEscalar produto1,produto2;
+public class CossenoTest {
+	Cosseno cosseno1, cosseno2;
 
 	List<Integer> opinioesUser1 = new ArrayList<Integer>();
 	List<Integer> opinioesUser2 = new ArrayList<Integer>();
@@ -29,7 +29,7 @@ public class ProdutoEscalarTest {
 	Usuario user3;
 
 	@Before
-	public void criaProdutosEscalar() throws Exception{
+	public void criaCossenos() throws Exception{
 		
 		opinioesUser1.add(2);
 		opinioesUser2.add(1);
@@ -37,22 +37,22 @@ public class ProdutoEscalarTest {
 		user1 = new Usuario("Reinaldo",opinioesUser1);
 		user2 = new Usuario("Livia",opinioesUser2);
 		user3 = new Usuario("Raquel", opinioesUser3);
-		produto1 = new ProdutoEscalar(user1, user2);
+		cosseno1 = new Cosseno(user1, user2);
 		
 	}
 
 	@Test
 	public void testaConstrutor(){
-		
+
 		try{
-			new ProdutoEscalar(null, user2);
+			new Cosseno(null, user2);
 			Assert.fail("Esperava erro");
 		}catch(Exception ex){
 			Assert.assertEquals("Erro no construtor", "Usuario null.",ex.getMessage());
 		}
 
 		try{
-			new ProdutoEscalar(user1, null);
+			new Cosseno(user1, null);
 			Assert.fail("Esperava erro");
 		}catch(Exception ex){
 			Assert.assertEquals("Erro no construtor", "Usuario null.",ex.getMessage());
@@ -64,22 +64,22 @@ public class ProdutoEscalarTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Assert.assertEquals("Erro no construtor", user1,produto1.getUser1());
-		Assert.assertEquals("Erro no construtor", user2,produto1.getUser2());
+		Assert.assertEquals("Erro no construtor", user1,cosseno1.getUser1());
+		Assert.assertEquals("Erro no construtor", user2,cosseno1.getUser2());
 
 	}
 	@Test
 	public void testaSetUser1() throws Exception{
-
+		
 		try{
-			produto1.setUser1(null);
+			cosseno1.setUser1(null);
 			Assert.fail("Esperava erro");
 		}catch(Exception ex){
 			Assert.assertEquals("Usuario null.", ex.getMessage());
 		}
 		user3.setDataHoraCadastro("12:12:12 12:12:12");
-		produto1.setUser1(user3);
-		Assert.assertEquals("Erro no metodo setUser1", user3,produto1.getUser1());
+		cosseno1.setUser1(user3);
+		Assert.assertEquals("Erro no metodo setUser1", user3,cosseno1.getUser1());
 
 	}
 
@@ -87,41 +87,41 @@ public class ProdutoEscalarTest {
 	public void testaSetUser2() throws Exception{
 
 		try{
-			produto1.setUser2(null);
+			cosseno1.setUser2(null);
 			Assert.fail("Esperava erro");
 		}catch(Exception ex){
 			Assert.assertEquals("Usuario null.", ex.getMessage());
 		}
 		user3.setDataHoraCadastro("12:12:12 12:12:12");
-		produto1.setUser2(user3);
-		Assert.assertEquals("Erro no metodo setUser1", user3,produto1.getUser2());
+		cosseno1.setUser2(user3);
+		Assert.assertEquals("Erro no metodo setUser2", user3,cosseno1.getUser2());
 		
 	}
 	
 	@Test
 	public void testaGetResultado(){
 		
-		Assert.assertEquals("Erro no metodo getResultado", 0.0,produto1.getResultado(), 0.0005);
-		produto1.calculaSimilaridade();
-		Assert.assertEquals("Erro no metodo getResultado", 2.0,produto1.getResultado(), 0.0005);
+		Assert.assertEquals("Erro no metodo getResultado", 0.0,cosseno1.getResultado(), 0.0005);
+		cosseno1.calculaSimilaridade();
+		Assert.assertEquals("Erro no metodo getResultado", 1.0,cosseno1.getResultado(), 0.0005);
 		
 	}
 	@Test
 	public void testaSetResultado(){
 		
-		Assert.assertEquals("Erro no metodo setResultado", 0.0,produto1.getResultado(), 0.0005);
-		produto1.setResultado(5);
-		Assert.assertEquals("Erro no metodo setResultado", 5.0,produto1.getResultado(), 0.0005);
+		Assert.assertEquals("Erro no metodo setResultado", 0.0,cosseno1.getResultado(), 0.0005);
+		cosseno1.setResultado(5);
+		Assert.assertEquals("Erro no metodo setResultado", 5.0,cosseno1.getResultado(), 0.0005);
 		
 	}
 	@Test
 	public void testaCompareTo() throws Exception{
 		
-		produto2 = new ProdutoEscalar(user1, user3);
-		produto2.calculaSimilaridade();
-		produto1.calculaSimilaridade();
-		Assert.assertEquals("Erro no metodo compareTo", -1, produto1.compareTo(produto2));
-		Assert.assertEquals("Erro no metodo compareTo", 1, produto2.compareTo(produto1));
+		cosseno2 = new Cosseno(user1, user3);
+		cosseno2.calculaSimilaridade();
+		cosseno1.calculaSimilaridade();
+		Assert.assertEquals("Erro no metodo compareTo", 0, cosseno1.compareTo(cosseno2));
+		Assert.assertEquals("Erro no metodo compareTo", 0, cosseno2.compareTo(cosseno1));
 		
 	}
 }
